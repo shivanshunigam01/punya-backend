967610ed05f2f24927c7276238f44fa4e30d1c6f
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2026-02-05 17:00:29 +0530

    asd

--- src/app.js
+++ src/app.js
@@ -30,18 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 
 /* cors */
-app.use(
-  cors({
-    origin: [
-      "https://patliputragroup.co.in",
-      "https://cpanel.patliputragroup.com",
-      "http://localhost:8080",
-      "http://localhost:8081",
-      "https://patliputragroup.com/"
-    ],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(morgan("dev"));
 
