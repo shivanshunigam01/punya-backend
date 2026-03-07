6225dded154404d516522f2ea4d04912cc82e910
shivashunigam01 <shivanshunigam8@gmail.com>
2026-03-07 13:38:35 +0530

    end changes

--- src/app.js
+++ src/app.js
@@ -23,8 +23,8 @@ app.use(mongoSanitize());
 app.use(hpp());
 
 /* parsers */
-app.use(express.json({ limit: "2mb" }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 /* perf */
 app.use(compression());
