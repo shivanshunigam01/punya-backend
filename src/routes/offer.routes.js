978fc254cd48e6b9adceb60457578897711c5e32
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2026-03-17 03:01:51 +0530

    test

--- src/routes/offer.routes.js
+++ src/routes/offer.routes.js
@@ -19,7 +19,7 @@ router.use(requireAuth);
 /**
  * ADMIN / MASTER ADMIN
  */
-router.get("/", requireRole(["master_admin", "master_admin"]), listOffers);
+router.get("/"), listOffers);
 router.get("/:id", requireRole(["master_admin", "master_admin"]), getOffer);
 router.post("/", requireRole(["master_admin", "master_admin"]), createOffer);
 router.put("/:id", requireRole(["master_admin", "master_admin"]), updateOffer);
