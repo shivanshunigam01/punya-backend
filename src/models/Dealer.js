f9ca522f63000cde3627b127cafbc953625bd093
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2026-03-18 14:13:45 +0530

    test

--- src/models/Dealer.js
+++ src/models/Dealer.js
@@ -1,3 +1,34 @@
+// import mongoose from "mongoose";
+
+// const dealerSchema = new mongoose.Schema(
+//   {
+//     name: { type: String, required: true },
+//     address: { type: String, required: true },
+
+//     city: { type: String, required: true },
+//     district: { type: String, required: true },
+//     state: { type: String, required: true },
+//     pincode: { type: String, required: true },
+
+//     phone: { type: String, required: true },
+//     whatsapp: { type: String },
+//     email: { type: String },
+//     brand: {
+//   type: String,
+//   enum: ["JCB", "Ashok Leyland", "Switch EV"],
+//   required: true,
+// },
+
+
+//     latitude: { type: Number, default: 0 },
+//     longitude: { type: Number, default: 0 },
+
+//     isActive: { type: Boolean, default: true },
+//   },
+//   { timestamps: true }
+// );
+
+// export const Dealer = mongoose.model("Dealer", dealerSchema);
 import mongoose from "mongoose";
 
 const dealerSchema = new mongoose.Schema(
@@ -13,12 +44,12 @@ const dealerSchema = new mongoose.Schema(
     phone: { type: String, required: true },
     whatsapp: { type: String },
     email: { type: String },
-    brand: {
-  type: String,
-  enum: ["JCB", "Ashok Leyland", "Switch EV"],
-  required: true,
-},
 
+    brand: {
+      type: String,
+      enum: ["JCB", "Ashok Leyland", "Switch EV"],
+      required: true,
+    },
 
     latitude: { type: Number, default: 0 },
     longitude: { type: Number, default: 0 },
